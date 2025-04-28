14d6af1f4f12a1f3845a31569832289e5a05905f
rizkisahat996 <rizkisitumeang77@gmail.com>
2025-04-28 21:41:54 +0700

    wedding-fix2

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com", "api.iconify.design", "www.transparenttextures.com"],
-  },
-  env: {
-    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
-    AUTH_SECRET: process.env.AUTH_SECRET,
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.iconify.design'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.transparenttextures.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
